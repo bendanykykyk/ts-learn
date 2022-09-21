ecf6eb0843d797624022bcab8d68350a92176ea1
bendanykykyk <446679223@qq.com>
2022-09-21 13:23:53 +0800

    ✨ feat: 剩下基本原始类型的补充以及非原始数据类型的引入

--- 03_total_practice/index.ts
+++ 03_total_practice/index.ts
@@ -1,3 +1,4 @@
+/************** 1.1原始数据类型 *******************/
 //  布尔值
 let isDone: boolean = false;
 // 数字
@@ -23,3 +24,40 @@ interface Obj {
 }
 let obj: Obj = {name: "yk", age: 18};
 // console.log(obj.) 出对应的提示
+
+// void 表示没有任何类型 跟any相对
+function warnUser(): void {
+  console.log("This is my warning message");
+}
+
+// null和undefined 是所有类型的子类型
+//*  let u: undefined = 3; // （虽然undefined是number的子类型，本身不会报错，但是tsconfig配置会有问题）
+let u = undefined; // 观察类型 u的类型其实是any
+
+// never 表示执行不完，肯定有错
+function error(message: string) {
+  throw new Error(message);
+}
+
+// 推断的返回值类型为never
+function fail() {
+  return error("Something failed");
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+  while (true) {}
+}
+/**************           *******************/
+
+/**************    1.2 非原始数据类型       *******************/
+// Object 对象
+declare function create(o: object): void;
+create({name: "0"});
+//* create(null);
+//* create(3);
+//* create("string");
+//* create(true);
+//* create(undefined);
+
+/**************           *******************/
