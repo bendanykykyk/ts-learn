76111f272369b341fc941e39426e17d68d4634e7
bendanykykyk <446679223@qq.com>
2022-09-21 12:48:55 +0800

    ✨ feat:基本数据类型-any

--- 03_total_practice/index.ts
+++ 03_total_practice/index.ts
@@ -11,3 +11,15 @@ let age: number = 37;
 let sentence: string = `Hello, my name is ${name}.
 
 I'll be ${age + 1} years old next month.`;
+
+// any 不确定变量的类型
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false;
+// any 给不到提示
+interface Obj {
+  name: string;
+  age: number;
+}
+let obj: Obj = {name: "yk", age: 18};
+// console.log(obj.) 出对应的提示
