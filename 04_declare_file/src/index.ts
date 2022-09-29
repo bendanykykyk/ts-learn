97fdd9e4130cfe4a62929b9621c2569f8e9c80b5
bendanykykyk <446679223@qq.com>
2022-09-29 10:18:54 +0800

    申明文件 - 基本举例

--- 04_declare_file/src/index.ts
+++ 04_declare_file/src/index.ts
@@ -1,5 +1,9 @@
 console.log(jQuery("#box"));
 // greet("3");
+
+function getWidget(str: string) {
+  return [str];
+}
 let items: string[] = getWidget("all of them");
 let item: number = getWidget(2);
 
@@ -8,6 +12,15 @@ let item: number = getWidget(2);
 //   duration?: number;
 //   color?: string;
 // }
+
 let greetingSettings: GreetingSettings = {
   greeting: "hello",
 };
+
+// 带属性的对象
+let result = myLib.api;
+
+let animal = new Animal("青蛙");
+console.log(animal.attack);
+
+greet("2");
--- 04_declare_file/src/types/index.d.ts
+++ 04_declare_file/src/types/index.d.ts
@@ -13,3 +13,20 @@ interface GreetingSettings {
 }
 
 declare function createGreetingSettings(setting: GreetingSettings): void;
+
+// 带属性的对象
+declare namespace myLib {
+  let api: (params) => void;
+}
+
+declare class Animal {
+  name: string;
+  attack(): void;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+// 可重用类型(类型别名)
+type GreetingLike = string | (() => string);
+
+declare function greet(g: GreetingLike): void;
